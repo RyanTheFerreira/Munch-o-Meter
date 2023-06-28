d708bcd1535d2a08a27b850a9ba85413784755d6
Austin W <auswaller@gmail.com>
2023-06-28 18:49:52 -0400

    added seed data for FoodInfo and Users

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,23 @@
 const sequelize = require('../config/connection');
 
 const seedFood = require('./foodData');
+const seedUser = require('./userData');
 
+/*
 const userData = require('./userData.json');
 const { User } = require('../models');
+*/
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-
+  /*
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
+  */
+ 
+  await seedUser();
   await seedFood();
 
   process.exit(0);
