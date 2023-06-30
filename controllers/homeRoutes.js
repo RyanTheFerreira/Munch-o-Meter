fc97b0ab4cfbade539a59023bdf9f27a014a1215
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-06-29 21:45:11 -0400

    modified the nutrition table and sections

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,5 +54,13 @@ router.get('/nutrition', withAuth, (req, res) => {
   });
 });
 
+// Add a new route for the about page
+router.get('/about', withAuth, (req, res) => {
+  res.render('about', {
+    // Pass any necessary data to the nutrition template
+    // logged_in: req.session.logged_in,
+  });
+});
+
 
 module.exports = router;
