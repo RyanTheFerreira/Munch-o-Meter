230e6632201edcbd507805ee9f0ef2f63aedb7fe
Austin W <auswaller@gmail.com>
2023-07-05 20:14:22 -0400

    added nutrition.js to add meals to database

--- public/js/nutrition.js
+++ public/js/nutrition.js
@@ -2,12 +2,36 @@ const foodName = document.getElementById('name');
 const servingSize = document.getElementById('serving-size');
 const date = document.getElementById('date');
 const mealType = document.getElementById('meal-type');
+const addFoodBtn = document.getElementById('add-meal-btn');
 
 const addFood = async (event) => {
+  event.preventDefault();
   try {
-    
+    const name = foodName.value.trim();
+    const serving_amount = servingSize.value.trim();
+    const date_time = date.value.trim();
+    const meal_type = mealType.value.trim();
+    //const user_id = sess.user_id;
+
+    console.log(name, serving_amount, date_time, meal_type);
+
+    if (name && serving_amount && date_time && meal_type) {
+      const response = await fetch('/api/food/addfood', {
+        method: 'POST',
+        body: JSON.stringify({ name, serving_amount, date_time, meal_type }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        //document.location.replace('/');
+        console.log(name, serving_amount, date_time, meal_type);
+      } else {
+        alert('Failed to add food');
+      }
+    }
   }
   catch (err) {
     console.log(err);
   }
 };
+
+addFoodBtn.addEventListener('click', addFood);
